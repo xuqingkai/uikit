67a81289277660b63a839d630068a0acbe1086a4
xuqingkai <id@xuqingkai.com>
2023-04-14 23:53:16 +0800

    init

---
+++ js/test.js
@@ -0,0 +1,7 @@
+
+// add style
+document.writeln('<link rel="stylesheet" href="./css/uikit.css">');
+
+// add javascript
+document.writeln('<script src="./js/uikit.js"></script>');
+document.writeln('<script src="./js/uikit-icons.js"></script>');
